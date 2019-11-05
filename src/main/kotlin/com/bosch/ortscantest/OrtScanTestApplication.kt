a9b7e8b63ca9f657cffd27d51ea909d12b6dce82
Magnus Niemann <magnus.niemann@bosch-si.com>
2019-11-05 13:46:24 +0100

    first commit

---
+++ src/main/kotlin/com/bosch/ortscantest/OrtScanTestApplication.kt
@@ -0,0 +1,11 @@
+package com.bosch.ortscantest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrtScanTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrtScanTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/bosch/ortscantest/OrtScanTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bosch.ortscantest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrtScanTestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
